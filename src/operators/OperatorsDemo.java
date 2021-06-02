a7901a8a8d9b48baf392a5302b9400ce09f40fbe
vaibhav <codekul.vaibhav@gmail.com>
2021-06-02 09:29:35 +0530

    Operators

---
+++ src/operators/OperatorsDemo.java
@@ -0,0 +1,47 @@
+package operators;
+
+public class OperatorsDemo {
+//arithmetic operator -> +,-,*,/,%
+//unary operator -> ++,--, pre increment, post increment ,pre decrement,post decrement
+//assignment operator -> =, ==,!=,>=,<=
+//bitwise operator -> & |
+//Logical -> && ||
+
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 7;
+
+        double k= i/j;
+//        int a = i%j;
+
+//        System.out.println(a);
+        System.out.println(k);
+
+//        i = i+1;
+//        System.out.println(i++);11(12)
+//        System.out.println(++i);
+//        i=i-1;
+//        System.out.println(i--);
+//        System.out.println(--i);
+
+        String str = "java";
+        String str1 = "java";
+
+//        int a = 10;1010
+//        int b = 7;|0111
+//                   1111
+////
+////        System.out.println(str != str1);
+//
+//        System.out.println(a & b);
+//
+//        System.out.println(a | b);
+
+        int a =10;
+        int b = 20;
+        System.out.println(b>a && a<b);
+        System.out.println(a>b || a>b);
+
+    }
+}
