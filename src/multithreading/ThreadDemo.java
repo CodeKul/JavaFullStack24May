e6a503043b71aee1f916c9f53329a9d3f1b387e4
vaibhav <codekul.vaibhav@gmail.com>
2021-06-24 09:34:23 +0530

    Multithreading

---
+++ src/multithreading/ThreadDemo.java
@@ -0,0 +1,54 @@
+package multithreading;
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+
+        Hii obj = new Hii();
+        Thread t1= new Thread(obj);
+        t1.start();
+//        obj.run();
+
+        Hello obj1 = new Hello();
+        obj1.start();
+    }
+}
+
+class Hii implements Runnable{
+
+    @Override
+    public void run(){
+        for (int i=1;i<=5;i++){
+            System.out.println("Hii");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hello extends Thread{
+
+    @Override
+    public void run() {
+        for (int i=1;i<=5;i++){
+            System.out.println("Hello");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+//new => when you create object of thread and before invocation of start().
+//runnable => when start method is invoked but thread scheduler is not selected as running thread
+//running => when thread scheduler is selected as running thread.
+//Blocking => when thread is waiting or sleep that time thread is in blocking state
+//died => after exit of run method
+
+
+
